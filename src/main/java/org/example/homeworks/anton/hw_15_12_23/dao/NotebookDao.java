03f5b40ddc1d9d59b246b8f912a91aaeef4f8556
anton gvozdenko <antongvozdenko1994@gmail.com>
2023-12-22 16:13:27 +0200

    homework

--- src/main/java/org/example/homeworks/anton/hw_15_12_23/dao/NotebookDao.java
+++ src/main/java/org/example/homeworks/anton/hw_15_12_23/dao/NotebookDao.java
@@ -1,8 +1,26 @@
 package org.example.homeworks.anton.hw_15_12_23.dao;
 
+import org.example.homeworks.anton.hw_15_12_23.domain.Notebook;
+
 import java.sql.SQLException;
 
 public interface NotebookDao {
     public void showById(int id) throws SQLException;
+
+    public void showByModel(String model) throws SQLException;
+
+    public void showAll() throws SQLException;
+
+    public void removeById(int id) throws SQLException;
+
+    public void removeAll() throws SQLException;
+
+    public void showByDate(String date) throws SQLException;
+
+    public void showBySsdRam(int ram, int ssd) throws SQLException;
+
+    public void showByCpu(String cpu) throws SQLException;
+
+    public void updateById(Notebook notebook, int id) throws SQLException;
 
 }
--- src/main/java/org/example/homeworks/anton/hw_15_12_23/service/impl/NotebookServiceImpl.java
+++ src/main/java/org/example/homeworks/anton/hw_15_12_23/service/impl/NotebookServiceImpl.java
@@ -1,14 +1,55 @@
 package org.example.homeworks.anton.hw_15_12_23.service.impl;
 
 import org.example.homeworks.anton.hw_15_12_23.dao.NotebookDao;
+import org.example.homeworks.anton.hw_15_12_23.dao.impl.NotebookDAOImpl;
+import org.example.homeworks.anton.hw_15_12_23.domain.Notebook;
 import org.example.homeworks.anton.hw_15_12_23.service.NotebookService;
 
 import java.sql.SQLException;
 
 public class NotebookServiceImpl implements NotebookService {
     private NotebookDao dao;
+
+    public NotebookServiceImpl() {
+        dao = new NotebookDAOImpl();
+    }
+
     @Override
     public void showById(int id) throws SQLException {
         dao.showById(id);
+    }
+
+    @Override
+    public void showByModel(String model) throws SQLException {
+        dao.showByModel(model);
+    }
+
+    @Override
+    public void showAll() throws SQLException {
+        dao.showAll();
+    }
+
+    public void removeById(int id) throws SQLException {
+        dao.removeById(id);
+    }
+
+    public void removeAll() throws SQLException {
+        dao.removeAll();
+    }
+
+    public void showByDate(String date) throws SQLException {
+        dao.showByDate(date);
+    }
+
+    public void showBySsdRam(int ram, int ssd) throws SQLException {
+        dao.showBySsdRam(ram, ssd);
+    }
+
+    public void showByCpu(String cpu) throws SQLException {
+        dao.showByCpu(cpu);
+    }
+
+    public void updateById(Notebook notebook, int id) throws SQLException {
+        dao.updateById(notebook, id);
     }
 }
